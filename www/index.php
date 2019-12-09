b8dd31ed7f6ca5428d8533f71a4bc316a7ee9e24
Kazuki Matsui <>
2019-12-09 18:31:56 +0900

    datepicker連携

--- www/index.php
+++ www/index.php
@@ -1,74 +1,96 @@
+<?php
+    save();
+    function save()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+            //配列の整理
+            $data = [];
+            foreach ($_POST['route'] as $date => $detail) {
+                $detail_array = [];
+                foreach ($detail as $k => $v) {
+                    $detail_array[] = [
+                        'id' => $k,
+                        'time' => $v['time'],
+                        'place' => $v['place'],
+                    ];
+                }
+                unset($k, $v);
+
+                $data[] = [
+                    'date' => $date,
+                    'detail' => $detail_array
+                ];
+            }
+            unset($v);
+
+            //json保存
+            $data = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            $json = fopen('./data/save.json', 'w+b');
+            fwrite($json, $data);
+            fclose($json);
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
     <div class="row justify-content-center mt-3">
         <div class="col-8">
-            <form class="row">
+            <form class="row" action="<?php print $_SERVER['SCRIPT_NAME']; ?>" method="post">
                 <div id="app" class="col-12">
-                    <div class="row d-flex justify-content-between form-group mb-3">
-                        <date-field
-                            v-for="item in route_input"
-                            v-bind:todo="item"
-                            v-bind:key="item.id"
-                        ></date-field>
+                    <div class="form-group col-12 mb-5">
+                        <label>予定日</label>
+                        <div class="row justify-content-between">
+                            <flat-pickr
+                                @on-change="change_date('start', $event)"
+                                :config="config"
+                                v-model="start"
+                                class="form-control col-5" 
+                                placeholder="開催日を選択してください"
+                                name="period[]">
+                            </flat-pickr>
+                            <flat-pickr
+                                @on-change="change_date('end', $event)"
+                                :config="config"
+                                v-model="end"
+                                class="form-control col-5" 
+                                placeholder="終了日を選択してください"
+                                name="period[]">
+                            </flat-pickr>
+                            <span class="col-1"></span>
+                        </div>
+                        <p v-html="date_error">&nbsp;</p>
+                    </div>
+                    <date-field
+                        v-for="item in route_input"
+                        v-bind:todo="item"
+                        v-bind:key="item.id"
+                    ></date-field>
+                    <div class="row justify-content-center">
+                        <button type="submit" class="btn btn-primary">保存</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 
-    <?php
-        $data = [
-            [
-                'date' => 1,
-                'detail' => [
-                    [
-                        'id' => 0,
-                        'time' => '10:00',
-                        'place' => 'テキスト1',
-                    ],
-                    [
-                        'id' => 1,
-                        'time' => '11:00',
-                        'place' => 'テキスト2',
-                    ],
-                    [
-                        'id' => 2,
-                        'time' => '12:00',
-                        'place' => 'テキスト3',
-                    ]
-                ]
-            ],
-            [
-                'date' => 2,
-                'detail' => [
-                    [
-                        'id' => 0,
-                        'time' => '10:00',
-                        'place' => 'テキスト1',
-                    ]
-                ]
-            ]
-        ];
-        printf('<script> var data_array = %s </script>', json_encode($data)) ;
+
+    <?php 
+        //json取得
+        if (file_exists('./data/save.json')) {
+            $data = file_get_contents('./data/save.json');
+        } else {
+            $data = '[]';
+        }
+        printf('<script> let data_array = %s </script>', $data);
     ?>
-
-    <div id="test">
-        <form>
-            <input type="text" v-model="start" v-on:change="start_change($event)">
-            <input type="text" v-model="end" v-on:change="end_change($event)">
-        </form>
-
-        {{ period }}
-    </div>
-
     <script src="/js/app.js"></script>
 </body>
 </html>
--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,21 +1,26 @@
-
-var moment = require("moment");
 window.Vue = require('vue')
+const moment = require("moment");
+require('bootstrap-css-only')
 
+
+//flatPickr
+var flatPickr = require('vue-flatpickr-component')
+require('flatpickr/dist/flatpickr.css')
+
+
+//component
 Vue.component('date-field', {
   props: ['todo'],
   template: `
-  <div class="col-12 mb-3">
-    <p>{{ todo.date }}日目</p>
+  <div class="form-group col-12 mb-5">
+    <label>{{ todo.date }}日目</label>
     <detail-field
       v-for="item in todo.detail"
       v-bind:detail="item"
       v-bind:key="item.id"
       v-bind:date="todo.date"
     ></detail-field>
-    <div class="row d-flex justify-content-around form-group">
-      <div @click="add_route(todo)" class="btn btn-info col-2">ルートの追加</div>
-    </div>
+    <div @click="add_route(todo)" class="text-center btn btn-info mt-4">ルートの追加</div>
   </div>`,
   methods: {
     add_route: function (todo) {
@@ -25,14 +30,13 @@ Vue.component('date-field', {
     }
   }
 })
-
 Vue.component('detail-field', {
   props: ['date', 'detail'],
   template: `
-  <div class="row">
-    <input type="text" v-bind:value="detail.place" v-bind:name="set_name(date, detail, 'place')">
-    <input type="time" v-bind:value="detail.time" v-bind:name="set_name(date, detail, 'time')">
-    <span @click="remove_route(detail, date)">削除</span>
+  <div class="row mb-3 justify-content-between">
+    <input class="form-control col-5" type="text" v-bind:value="detail.place" v-bind:name="set_name(date, detail, 'place')">
+    <input class="form-control col-5" type="time" v-bind:value="detail.time" v-bind:name="set_name(date, detail, 'time')">
+    <span class="btn btn-light col-1" @click="remove_route(detail, date)">削除</span>
   </div>
   `,
   methods: {
@@ -43,6 +47,9 @@ Vue.component('detail-field', {
       key = date - 1
       new_array = []
       var c = 0
+      if (app.route_input[key]['detail'].length <= 1) {
+        return;
+      }
       app.route_input[key]['detail'].forEach(function (element, i) {
         if (detail.id != i) {
           new_array[c] = {
@@ -58,33 +65,60 @@ Vue.component('detail-field', {
   }
 })
 
+
+//インスタンス
 var app = new Vue({
   el: '#app',
   data: {
     route_input: data_array,
-    period: 1
-  }
-})
-
-
-
-
-
-var test = new Vue({
-  el: '#test',
-  data: {
-    start: moment().format("YYYY-MM-DD"),
-    end: moment().format("YYYY-MM-DD"),
-    period: moment(1).format("DDDD")
+    start: null,
+    end: null,
+    period: null,
+    config: {
+      minDate: "today"
+    },
+    date_error: "&nbsp"
+  },
+  components: {
+    flatPickr
   },
   methods: {
-    start_change: function (e) {
-      this.period = moment(this.end) - moment(e.target.value)
-      this.period = moment(this.period).format("DDDD");
-    },
-    end_change: function (e) {
-      this.period = moment(e.target.value) - moment(this.start)
-      this.period = moment(this.period).format("DDDD");
+    change_date: function (type, e) {
+
+      if (type == 'start') {
+        this.start = moment(e[0]).format("YYYY-MM-DD");
+      } else {
+        this.end = moment(e[0]).format("YYYY-MM-DD");
+      }
+      this.period = moment(this.end).diff(moment(this.start), 'days')
+
+      //エラーの場合
+      if (this.start && this.end) {
+
+        if (this.period < 0) {
+          this.date_error = '<span class="text-danger">正しい日付を選択してください。</span>'
+          return;
+        } else {
+          this.date_error = "&nbsp"
+        }
+
+        //追加
+        for (var i = 0; i <= this.period; i++) {
+          if (this.route_input[i] === void 0) {
+            this.route_input.push({
+              date: i + 1,
+              detail: [{
+                id: 0,
+                time: '',
+                place: ''
+              }]
+            })
+          }
+        }
+
+        //削除
+        this.route_input.splice(this.period + 1)
+      }
     }
   }
 })
